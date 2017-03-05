5de86d7f7cadf42f11fef105c4db5cc97bfc9f68
Serge Shirokov <shirokov.s@ati.su>
2017-03-05 13:37:55 +0300

    Убрал пошаговую отрисовку (была для видео)

--- src/app/constants.js
+++ src/app/constants.js
@@ -3,5 +3,5 @@ export const LOAD_PATH_FAILED = 'LOAD_PATH_FAILED';
 export const START_ROAD_LOADED = 'START_ROAD';
 export const START_ROAD_FAILED = 'START_ROAD';
 
-export const loadId = '180.675807748645';
+export const loadId = '180.6754539002504';
 export const apiServer = 'http://ec43419d.ngrok.io';
--- src/stores/map.js
+++ src/stores/map.js
@@ -1,8 +1,6 @@
 import * as constants from 'app/constants';
 import Fluxxor from 'fluxxor';
 
-var maxPoint = 1;
-
 let map = Fluxxor.createStore({
     initialize: function () {
         this.state = {};
@@ -16,10 +14,7 @@ let map = Fluxxor.createStore({
             return a.time < b.time;
         });
 
-        var limitedPoints = sortedPoints.slice(0, maxPoint);
-        maxPoint++;
-
-         this.setProps({ 'points': limitedPoints});
+         this.setProps({ 'points': sortedPoints});
     },
 
     setProps(obj) {
