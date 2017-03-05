68e74ddb6b4a30716c865d0ab4811e866d352920
Makar Zabiyakin <makar.z@ati.su>
2017-03-05 11:50:25 +0300

    add view request photo

---
+++ src/components/RequestPhotoPanel/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Fluxxor from 'fluxxor';
+import styles from './style.css';
+
+let FluxMixin = Fluxxor.FluxMixin(React);
+
+let requestPhotoPanel = React.createClass({
+
+    requestPhotoClick() {
+        console.log("hello requestPhotoClick");
+    },
+
+    showContent() {
+        return <div>
+            <span className="contentTitle">{"Volvo FH16, м629ро86RUS"}</span><br />
+            <span>{"Крапивин Ильсур Болгарович +79814424524"}</span> <br />
+            <span>{"Мороженные помидоры 16т., насыпью"}</span>
+        </div>;
+    },
+
+    render: function () {
+        const content = this.showContent();
+        return (
+            <div className='panel'>
+                <div className='icon ic_car'></div>
+
+                <div className='content'>{content}</div>
+                <div className='right'>
+                    <button className='button' onClick={this.requestPhotoClick} >
+                        <span className='textButton'>ЗАПРОСИТЬ ФОТО</span>
+                    </button>
+                    <div className='costTitle'>Стоимость 100 ат.</div>
+                </div>
+            </div>);
+    },
+});
+
+export default requestPhotoPanel;
